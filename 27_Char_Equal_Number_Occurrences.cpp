d0ac155f07c056837ad99a560e59c67a6489d147
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-02-27 15:33:28 +0530

    Add files via upload

---
+++ 27_Char_Equal_Number_Occurrences.cpp
@@ -0,0 +1,44 @@
+// Given a string s, return true if s is a good string, or false otherwise.
+// A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+// Example 1:
+
+// Input: s = "abacbc"
+// Output: true
+// Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+// Example 2:
+
+// Input: s = "aaabb"
+// Output: false
+// Explanation: The characters that appear in s are 'a' and 'b'.
+// 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+ 
+
+
+
+
+
+
+
+// My approach and optimal too
+
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        int freq[26] = {0};
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        int ref = freq[s[0] - 'a'];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s[i] - 'a'] != ref) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
